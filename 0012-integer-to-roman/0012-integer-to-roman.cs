8b0ec38aae2c957fc55fc4a7203491ec540d4d91
Thato Omphile Nameng <71035775+Thato-Nameng@users.noreply.github.com>
2023-09-18 16:30:59 +0200

    Time: 52 ms (99.76%), Space: 43.6 MB (50.32%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cs
+++ 0012-integer-to-roman/0012-integer-to-roman.cs
@@ -2,12 +2,15 @@ public class Solution {
     public string IntToRoman(int num) {
         string[] Roman = {
             "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"
-        };                      
+        };
+        
         int[] numerals = {
             1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1        
-        };        
+        };    
+        
         string romanNumber = "";
-        int i = 0;        
+        int i = 0;  
+        
         do{
             while(num >= numerals[i]){
                 romanNumber += Roman[i];
