43852f7964b469e4d8b1f292f75a58a9946d474e
Thato Omphile Nameng <71035775+Thato-Nameng@users.noreply.github.com>
2023-09-20 16:15:24 +0200

    Time: 68 ms (54.26%), Space: 37.7 MB (84.70%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.cs
@@ -0,0 +1,23 @@
+public class Solution {
+    public bool WordPattern(string pattern, string s) {
+        string[] words = s.Split(' ');
+
+        if (pattern.Length != words.Length)
+            return false;
+        
+        for (int i = 0; i < pattern.Length; i++) {
+             char c = pattern[i];
+            string word = words[i];
+
+            for (int j = i + 1; j < pattern.Length; j++) {
+                if (pattern[j] == c && words[j] != word)
+                    return false;
+                if (pattern[j] != c && words[j] == word)
+                    return false;
+            }        
+        }
+                
+        return true;               
+    }
+    
+}
